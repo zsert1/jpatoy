f2c6e21b9e8cd54e1644c9bc8a432ef0ecd75ac1
zsert1 <zsert1@naver.com>
2024-07-11 00:24:48 +0900

    jap기본설정

---
+++ evaluation/src/test/java/com/soccerplay/evaluation/EvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soccerplay.evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
