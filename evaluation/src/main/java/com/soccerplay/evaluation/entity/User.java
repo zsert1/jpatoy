1b3a32526bc812895fa987d9f29861b5d84362d6
zsert1 <zsert1@naver.com>
2024-07-11 23:42:26 +0900

    에러수정

--- evaluation/src/main/java/com/soccerplay/evaluation/entity/User.java
+++ evaluation/src/main/java/com/soccerplay/evaluation/entity/User.java
@@ -1,12 +1,7 @@
 package com.soccerplay.evaluation.entity;
 
-import java.util.Collection;
-import java.util.UUID;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,48 +16,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Builder
-public class User  implements UserDetails{
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String name;
-
-    @Column(unique = true)
-    private String email;
-
+    private String username;
     private String password;
+    private String role;
 
-
-//    권한 체크
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
-    }
-
-    @Override
-    public String getUsername() {
-        return getEmail();
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return false;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return false;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
+    // Getters and Setters
 }
